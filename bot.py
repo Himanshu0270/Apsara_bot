e34dacb6f7980c2936e35746b41af6143c8e375e
Himanshu0270 <gamerhimanshu69@gmail.com>
2025-04-26 22:58:42 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import random
 import os
 
 # Load token from environment variable or you can directly paste your token
-TOKEN = os.getenv("TOKEN")  # <- For Railway Hosting
+TOKEN = os.getenv({TOKEN})  # <- For Railway Hosting
 #TOKEN = 'TOKEN'  # <- If you are running locally, uncomment this and paste your token here
 
 # Set Intents
