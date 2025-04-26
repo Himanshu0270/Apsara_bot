6bfc95e78cf3544fca8e4e62a3c148f73991b3d9
Himanshu0270 <gamerhimanshu69@gmail.com>
2025-04-26 22:43:21 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -5,8 +5,8 @@ import random
 import os
 
 # Load token from environment variable or you can directly paste your token
-TOKEN = os.getenv("TOKEN")  # <- For Railway Hosting
-# TOKEN = 'MTM2NTMxNDA4MzMzMDQ1NzYwMA.GNve29.Oz8UqHu-yd_oyzgq41ZsZZMDQ5Svgq7cOtGZdQ'  # <- If you are running locally, uncomment this and paste your token here
+#TOKEN = os.getenv("TOKEN")  # <- For Railway Hosting
+TOKEN = 'TOKEN'  # <- If you are running locally, uncomment this and paste your token here
 
 # Set Intents
 intents = discord.Intents.default()
